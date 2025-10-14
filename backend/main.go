acb270e3432ce86b223effe276de6644f9f82f13
aigerim-bi <aigerim.bissembayeva@narxoz.kz>
2025-10-15 00:39:27 +0500

    Front

---
+++ backend/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"ED/config"
+	"ED/routes"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/rs/cors"
+)
+
+func main() {
+	config.ConnectDatabase()
+
+	// создаем маршрутизатор
+	r := routes.SetupRouter()
+
+	// настраиваем CORS (разрешаем запросы с фронта на http://localhost:3000)
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: true,
+	})
+
+	// оборачиваем маршрутизатор
+	handler := c.Handler(r)
+
+	fmt.Println("Server running on port 8080 🚀")
+	log.Fatal(http.ListenAndServe(":8080", handler))
+}
---
+++ backend/routes/routes.go
@@ -0,0 +1,30 @@
+package routes
+
+import (
+	"ED/controllers"
+
+	"github.com/gorilla/mux"
+)
+
+func SetupRouter() *mux.Router {
+	r := mux.NewRouter()
+
+	// Users
+	r.HandleFunc("/users", controllers.CreateUser).Methods("POST")
+	r.HandleFunc("/users", controllers.GetUsers).Methods("GET")
+	r.HandleFunc("/users/{id}", controllers.GetUser).Methods("GET")
+	r.HandleFunc("/users/{id}", controllers.DeleteUser).Methods("DELETE")
+
+	// Categories
+	r.HandleFunc("/categories", controllers.CreateCategory).Methods("POST")
+	r.HandleFunc("/categories", controllers.GetCategories).Methods("GET")
+
+	// Tasks
+	r.HandleFunc("/tasks", controllers.CreateTask).Methods("POST")
+	r.HandleFunc("/tasks", controllers.GetTasks).Methods("GET")
+	r.HandleFunc("/tasks/{id}", controllers.GetTask).Methods("GET")
+	r.HandleFunc("/tasks/{id}", controllers.UpdateTask).Methods("PUT")
+	r.HandleFunc("/tasks/{id}", controllers.DeleteTask).Methods("DELETE")
+
+	return r
+}
